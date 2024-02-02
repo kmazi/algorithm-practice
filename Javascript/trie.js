8a871c31dc2653eafc39127b82325f66a272ea9a
mazimia <kingsleyu13@yahoo.com>
2024-02-02 12:47:51 +0100

    Print all words in javascript trie.

--- Javascript/trie.js
+++ Javascript/trie.js
@@ -16,17 +16,22 @@ class Trie {
         // Get node represented by character if it exists or create
         // a new node for it.
         const character = word[index];
-        const nextNode = currentNode.children.get(character);
+        let nextNode = currentNode.children.get(character);
         if (nextNode === undefined) {
             nextNode = currentNode.children.set(character, 
                 new TrieNode()).get(character);
         }
+        // increase word count and character index by 1
         nextNode.count += 1;
         index += 1;
 
-        if (index < len(word)) {
+        if (index < word.length) {
+            // Go over same process for the rest of the characters in
+            // the word.
             this.insertWord(nextNode, word=word, index=index);
         } else {
+            //At the last character of the word. set that word attribute
+            // to true to indicate a word ends there.
             nextNode.word = true;
         }
 
@@ -65,12 +70,49 @@ class Trie {
     }
 
     search(currentNode, word, index=0, prefix=true) {
+        // Get node at current character position.
+        // word doesn't exist if no node is present at character position.
         currentNode = currentNode.children.get(word[index]);
         if (currentNode === undefined) return false;
         index += 1;
 
+        // Go over process again with the next node until that last
+        // character of word is reached.
+        // If the last character of search string is a word or prefix of word
+        // print true or false otherwise.
         if (index < word.length) {
             return this.search(currentNode, word, index, prefix);
         } else return prefix || currentNode.word;
     }
+
+    printWords(currentNode, word='') {
+        // Go through each every node from root node downward
+        currentNode.children.forEach((node, character) => {
+            if (node.word) {
+                // print word when end of character node is met
+                console.log(word+character);
+                // continue search if end of character node is a prefix
+                // of another word.
+                if (node.count > 1) {
+                    this.printWords(node, word+character);
+                }
+            } else {// continue search until end of node is reached.
+                this.printWords(node, word+character);
+            }
+        });
+    }
 }
+
+
+const trie = new Trie();
+const words = ['the', 'alaba', 'alabasta', 'albeit', 'alpine', 'a', 'ab', 'worse', 'always', 'allwell', 'alive', 'w'];
+words.forEach((word) => {
+    trie.insertWord(trie.root, word, 0);
+});
+
+trie.printWords(trie.root);
+console.log('Searching for alarm:', trie.search(trie.root, 'alarm'));
+console.log('Searching for alaba:', trie.search(trie.root, 'alaba'));
+console.log('Searching for alab:', trie.search(trie.root, 'alab'));
+console.log('Deleting w:', trie.deleteWord(trie.root, 'w'));
+console.log('Searching for deleted w:', trie.search(trie.root, 'w', prefix=false));
