e4e3b6c5241b8b540051d5d366bcc90b094695f2
mazimia <kingsleyu13@yahoo.com>
2024-02-02 12:48:14 +0100

    print all words in python trie.

--- Python/trie/trie_structure.py
+++ Python/trie/trie_structure.py
@@ -20,36 +20,51 @@ class Trie:
         """Set root val for trie."""
         self.root = TrieNode()
 
-    def insert(self, current_node: TrieNode, word: str, 
+    def insert(self, current_node: TrieNode, word: str,
                index: int = 0) -> None:
         """Add word to trie structure."""
-
+        # Create a new node for character if it doesn't already exist
+        # else return the node at that charater position.
         current_node = current_node.children.setdefault(
             word[index], TrieNode())
-        # increase word count by 1
+        # increase word count and character index by 1
         current_node.count += 1
-
         index += 1
 
         if index < len(word):
+            # Go over same process for the rest of the characters in
+            # the word.
             self.insert(current_node=current_node, word=word, index=index)
         else:
+            # At the last character of the word. set that word attribute
+            # to true to indicate a word ends there.
             current_node.word = True
 
-    def delete(self, current_node: TrieNode, word: str, index: int=0):
+    def delete(self, current_node: TrieNode, word: str, index: int = 0):
+        """Delete word in trie."""
         character = word[index]
         next_node = current_node.children.get(character)
+        # Next node will start from first character in word to the end.
+        # When the character is not found, word doesn't exist in storage.
         if next_node is None:
             return False
-        
+        # Increase index count to next character in word
         index += 1
         if index < len(word):
+            # call funx again to search more character if end of word
+            # isn't reached.
             self.delete(current_node=next_node, word=word, index=index)
         elif index == len(word) and next_node.word:
+            # If end of word to delete is reached and it exists in storage
+            # set it to false such that no word ends there anymore
             next_node.word = False
         else:
+            # if both conditions are not satisfied, then the word you're
+            # trying to delete is a prefix and such cannot be deleted.
             raise RecursionError('Not a word.')
-
+        # This is were deleting actually occurs.
+        # Reduce the count of word in character if it's used in other
+        # words. Delete character node otherwise.
         if next_node.count > 1:
             next_node.count -= 1
         else:
@@ -57,16 +72,36 @@ class Trie:
 
         return True
 
-    def search(self, current_node: TrieNode, word: str, index: int=0, 
+    def search(self, current_node: TrieNode, word: str, index: int = 0,
                prefix=True) -> bool:
         """Search trie storage for word."""
+        # Get node holding each character in word.
         current_node = current_node.children.get(word[index])
         index += 1
         if not current_node:
+            # This happens when no node is present for character
             return False
-        
+        # Continue search if not end of word
         if index < len(word):
-            return self.search(current_node=current_node, word=word, 
-                                index=index, prefix=prefix)
+            return self.search(current_node=current_node, word=word,
+                               index=index, prefix=prefix)
         else:
+            # return true if end of word is reached or word is a
+            # prefix else false
             return prefix or current_node.word
+
+    def print_words(self, current_node: TrieNode, word: str = ''):
+        """Print all words in storage."""
+        for character, node in current_node.children.items():
+            # Loop through every characters registered in node and print
+            # if a word ends there.
+            if node.word:
+                print(word+character)
+                # Continue to search down if word end is a prefix of
+                # another word.
+                if node.count > 1:
+                    self.print_words(node, word+character)
+            else:
+                # Call function again with next node if end of word is
+                # not reached yet.
+                self.print_words(node, word+character)
